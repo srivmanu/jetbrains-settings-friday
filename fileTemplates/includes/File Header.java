5ca88b9cddcff1ab3dcbdfac2e2f855bad249e83
Manu Srivastava <srivmanu@gmail.com>
2020-10-27 12:48:25 -0500

    refresh

--- fileTemplates/includes/File Header.java
+++
@@ -1,6 +0,0 @@
-/**
-* Created By ${USER} on ${DATE} for ${PROJECT_NAME}
-* This will always be a test run. 
-* Unless you are compiling to submit on play store. 
-* In which case, God help your soul.
-*/ 
